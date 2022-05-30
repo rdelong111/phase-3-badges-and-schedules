c44df4ca1e4af43f8be614e737f9a7e254918441
Ryan DeLong <rdelong111@gmail.com>
2022-05-29 21:09:23 -0500

    commit tree 396ce500ecab2ae26315941ea8db289da0646870

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  names.map {|name| badge_maker name}
+end
+
+def assign_rooms(names)
+  names.map.with_index(1) do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index}!"
+  end
+end
+
+def printer(names)
+  batch_badge_creator(names).each {|badge| puts badge}
+  assign_rooms(names).each {|assignment| puts assignment}
+end
